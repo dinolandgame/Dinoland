7d6884771a396a59bcf86dc35c73657f65ad00b0
XavierVicente <xvicente2000@gmail.com>
2017-03-17 22:51:52 +0100

    ahora si
    faltaba esta que habia hecho un revert

--- libs/routes.js
+++ libs/routes.js
@@ -3,7 +3,7 @@ Router.onBeforeAction(function(){
 //si no esta log
 	if(!Meteor.userId()){
 		//va a la ruta homeLogin
-		this.render('HomeLogin');
+		this.render('principal');
 
 		//finalizar el proceso de la peticion
 		this.next();
@@ -11,7 +11,7 @@ Router.onBeforeAction(function(){
 // si esta log
 	else{
 		//si esta en la ruta homeLogin llevalo a la ruta
-		if(Router.current().route.getName() === 'HomeLogin'){
+		if(Router.current().route.getName() === 'principal'){
 			Router.go('game');
 		}
 
@@ -26,32 +26,19 @@ Router.configure({
   waitOn: function(){
   	return Meteor.subscribe('partida');
   }
-		/*
-		waitOn: function() {
-				return	Meteor.user().subscription;
-
-			},
-		action: function(){
-				if (!this.ready()) {
-				  this.render('loading');
-				}
-				else {
-				   this.render('dinoGame', {to: 'dinoGame'});
-
-				}
-		}*/
+		
 });
 
 
 Router.route('/', function () {
 
-  this.render('HomeLogin');
+  this.render('principal');
 
 });
 
 
-Router.route('/HomeLogin', function () {
-  this.render('HomeLogin');
+Router.route('/principal', function () {
+  this.render('principal');
 });
 
 Router.route('/proyecto', function(){
@@ -70,15 +57,8 @@ Router.route('/nolog', function(){
 //****************** yield + template*************/
 
 Router.route('/game', function () {
-
-		this.layout('game');
-		Meteor.subscribe('edificio');
-
-		if(Meteor.userId()){
-			this.render('dinoGame', {to: 'dinoGame'});
-		}else{
-			this.render('Nolog', {to: 'nolog'});
-		}
+this.render('game')
+		
 });
 
 
@@ -102,18 +82,3 @@ Router.route( '/verify-email/:token', {
 
 });
 
-Router.route('/game/:_id', function () {
-  var params = this.params; // { _id: "5" }
-  var id = params._id;
-  if(Meteor.userId()){
-			this.render('dinoGame', {to: 'dinoGame'},{
-			    data: function () {
-			      return Edificio.findOne({_id: this.params._id});
-			    }
-			 });
-		}else{
-			this.render('Nolog', {to: 'nolog'});
-		}
-   
-   // "5"
-});
