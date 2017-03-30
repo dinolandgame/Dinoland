4858dc35face7076ed3bd910b5175d29ef12490e
XavierVicente <xvicente2000@gmail.com>
2017-03-30 11:19:50 +0200

    directo en la master
    implementacion del cron

---
+++ dinoGame.js
@@ -0,0 +1,68 @@
+Template.dinoGame.events({
+
+    "click #btn_desplegar":function(event,template){
+		event.preventDefault();
+		$('#btn_desplegar').hide();
+        $('#user').fadeOut(500);
+        //$.ionSound.play("snap");
+        $('#sidebar').addClass("sombra_sidebar");
+        $('#sidebar').animate({"left": "16.66666667%"}, "slow"); 
+        $('#btn_plegar').show();
+	},
+
+    "click #btn_plegar":function(event,template){
+		event.preventDefault();
+		//$.ionSound.play("snap");
+        $('#btn_plegar').hide();
+        $('#sidebar').animate({"left": "0%"}, "slow");
+        $('#sidebar').removeClass("sombra_sidebar");
+        $('#user').fadeIn(500);
+        $('#btn_desplegar').show();
+	},
+
+    "click a#home":function(event,template){
+			event.preventDefault();
+           
+			Router.go('/');
+            /*$('canvas').remove();
+            $('body').css("overflow","auto");*/
+        location.reload();
+	},
+
+    "click #user":function(event,template){
+        event.preventDefault();
+        $('#btn_desplegar').hide();
+        $('#user').fadeOut(500);
+       // $.ionSound.play("snap");
+        $('#sidebar').addClass("sombra_sidebar");
+        $('#sidebar').animate({"left": "16.66666667%"}, "slow"); 
+        $('#btn_plegar').show();
+    },
+    "onload window":function(event,template){
+        //game = new Phaser.Game(1400, 800, Phaser.CANVAS, 'dinosaur');
+        Router.go('game');
+    },
+
+    "click #subirlvl": function(event,template){
+            event.preventDefault();
+        
+            console.log("subir nivel");
+            
+            Edifici = Edificio.findOne({key:quinedifici});//busco los edicios; el edificio seleccionado es quinedifici
+            
+            Meteor.call('update_part',Edifici.key);        
+                
+            alert("se ha subido de nivel");
+
+            $('.modal').modal('hide');
+                    
+           
+        },
+     "click #mejorar": function(event,template){
+         
+            phaserEdifici.destroy();
+            game.state.restart();
+          }
+        
+ }); 
+
